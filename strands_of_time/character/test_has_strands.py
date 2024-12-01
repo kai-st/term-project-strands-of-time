01daf225fd570400e9c6742a669d06715f6b0463
Kai Steingarten <ksteingarten@my.bcit.ca>
2024-12-01 15:19:32 -0800

    Update unit tests for has_strands changes

--- strands_of_time/character/test_has_strands.py
+++ strands_of_time/character/test_has_strands.py
@@ -26,6 +26,28 @@ class TestHasStrands(TestCase):
         expected = True
         self.assertEqual(expected, actual)
 
+    def test_has_strands_strands_can_be_floats(self):
+        actual = character.has_strands({"X-coordinate": -1,
+                                        "Y-coordinate": -1,
+                                        "level": 1,
+                                        "last distance to goal": None,
+                                        "Strands": {"Red": 3.5, "Orange": 3.5, "Yellow": 3.5,
+                                                    "Green": 3.5,
+                                                    "Blue": 3.5, "Violet": 3.5}})
+        expected = True
+        self.assertEqual(expected, actual)
+
+    def test_has_strands_strands_can_mix_ints_and_floats(self):
+        actual = character.has_strands({"X-coordinate": -1,
+                                        "Y-coordinate": -1,
+                                        "level": 1,
+                                        "last distance to goal": None,
+                                        "Strands": {"Red": 3.5, "Orange": 3, "Yellow": 3.5,
+                                                    "Green": 3,
+                                                    "Blue": 3.5, "Violet": 3.5}})
+        expected = True
+        self.assertEqual(expected, actual)
+
     def test_has_strands_one_strand_colour_0(self):
         actual = character.has_strands({"X-coordinate": -1,
                                         "Y-coordinate": -1,
@@ -83,4 +105,42 @@ class TestHasStrands(TestCase):
                                                     "Green": -2, "Blue": -1, "Violet": -1}})
         expected = False
         self.assertEqual(expected, actual)
+
+    def test_has_strands_raises_type_error_if_character_not_dict(self):
+        with self.assertRaises(TypeError):
+            character.has_strands("X-coordinate")
+
+    def test_has_strands_raises_type_error_if_character_strands_not_dict(self):
+        with self.assertRaises(TypeError):
+            character.has_strands({"X-coordinate": -1,
+                                  "Y-coordinate": -1,
+                                   "level": 1,
+                                   "last distance to goal": None,
+                                   "Strands": "thing"})
+
+    def test_has_strands_raises_value_error_if_character_strands_all_values_not_numbers(self):
+        with self.assertRaises(ValueError):
+            character.has_strands({"X-coordinate": -1,
+                                  "Y-coordinate": -1,
+                                   "level": 1,
+                                   "last distance to goal": None,
+                                   "Strands": {"Red": "-1", "Orange": "-2", "Yellow": "-3",
+                                               "Green": "-2", "Blue": "-1", "Violet": "-1"}})
+
+    def test_has_strands_raises_value_error_if_character_strands_any_value_not_number(self):
+        with self.assertRaises(ValueError):
+            character.has_strands({"X-coordinate": -1,
+                                  "Y-coordinate": -1,
+                                   "level": 1,
+                                   "last distance to goal": None,
+                                   "Strands": {"Red": -1, "Orange": -2, "Yellow": -3,
+                                               "Green": -2, "Blue": -1, "Violet": "-1"}})
+
+    def test_has_strands_raises_key_error_if_character_has_no_strands_key(self):
+        with self.assertRaises(KeyError):
+            character.has_strands({"X-coordinate": -1,
+                                  "Y-coordinate": -1,
+                                   "level": 1,
+                                   "last distance to goal": None})
+
 
