865fcef6aca53e77f344b3bb4751e0374f9554b6
Kai Steingarten <ksteingarten@my.bcit.ca>
2024-12-01 17:29:38 -0800

    Move RAINBOW_PRINTING_SEQUENCES into colourize

--- strands_of_time/__init__.py
+++ strands_of_time/__init__.py
@@ -5,6 +5,3 @@ A01435070
 Strands of Time
 """
 RAINBOW_ORDER = ("Red", "Orange", "Yellow", "Green", "Blue", "Violet")
-
-RAINBOW_PRINTING_SEQUENCES = ("\033[38;5;160m", "\033[38;5;208m", "\033[38;5;220m",
-                              "\033[38;5;41m", "\033[38;5;33m", "\033[38;5;135m")
