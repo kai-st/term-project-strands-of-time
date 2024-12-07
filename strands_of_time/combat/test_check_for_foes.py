a944776b37534e66cff6aa05f5c130a3ab5a9914
Kai Steingarten <ksteingarten@my.bcit.ca>
2024-12-06 23:29:54 -0800

    Update tests for check_for_foes to percent_chance_result

--- strands_of_time/combat/test_check_for_foes.py
+++ strands_of_time/combat/test_check_for_foes.py
@@ -1,32 +1,37 @@
 from unittest import TestCase
 from unittest.mock import patch
 
-import strands_of_time.combat.combat
-from strands_of_time import game
+import combat
 
 
-class TestCheckForFoes(TestCase):
+class TestPercentageChanceResult(TestCase):
 
-    @patch('random.randint', return_value=1)
-    def test_check_for_foes_on_a_1(self, _):
-        actual = strands_of_time.combat.combat.check_for_foes()
+    @patch('random.randrange', return_value=99)
+    def test_percentage_chance_100_percent_chance(self, _):
+        actual = combat.percentage_chance_result(100)
+        expected = True
+        self.assertEqual(expected, actual)
+
+    @patch('random.randrange', return_value=0)
+    def test_percentage_chance_result_0_percent_chance(self, _):
+        actual = combat.percentage_chance_result(0)
         expected = False
         self.assertEqual(expected, actual)
 
-    @patch('random.randint', return_value=2)
-    def test_check_for_foes_on_a_2(self, _):
-        actual = strands_of_time.combat.combat.check_for_foes()
+    @patch('random.randrange', return_value=51)
+    def test_percentage_chance_result_50_percent_chance_failure(self, _):
+        actual = combat.percentage_chance_result(50)
         expected = False
         self.assertEqual(expected, actual)
 
-    @patch('random.randint', return_value=3)
-    def test_check_for_foes_on_a_3(self, _):
-        actual = strands_of_time.combat.combat.check_for_foes()
+    @patch('random.randrange', return_value=50)
+    def test_percentage_chance_result_50_percent_chance_fails_at_boundary_50(self, _):
+        actual = combat.percentage_chance_result(50)
         expected = False
         self.assertEqual(expected, actual)
 
-    @patch('random.randint', return_value=4)
-    def test_check_for_foes_on_a_4(self, _):
-        actual = strands_of_time.combat.combat.check_for_foes()
+    @patch('random.randrange', return_value=49)
+    def test_percentage_chance_result_50_percent_chance_success(self, _):
+        actual = combat.percentage_chance_result(50)
         expected = True
         self.assertEqual(expected, actual)
