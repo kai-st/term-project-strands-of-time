e1df97840dc2e2b82b67c33b4ceeaeb514cf1fb5
Kai Steingarten <ksteingarten@my.bcit.ca>
2024-12-06 22:51:25 -0800

    Update unit tests for describe_current_location

--- strands_of_time/location/test_describe_current_location.py
+++ strands_of_time/location/test_describe_current_location.py
@@ -2,7 +2,6 @@ import io
 from unittest import TestCase
 from unittest.mock import patch
 
-import game
 
 import strands_of_time.location.board
 
@@ -10,37 +9,37 @@ import strands_of_time.location.board
 class TestDescribeCurrentLocation(TestCase):
 
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_describe_current_location_initial_location(self, mock_stdout):
-        test_board = {(0, 0): "room description A",
-                      (1, 0): "room description B",
-                      (0, 1): "room description C",
-                      (1, 1): "room description D"
+    def test_describe_current_location_0_0(self, mock_stdout):
+        test_board = {(0, 0): {"description": "room description A"},
+                      (1, 0): {"description": "room description B"},
+                      (0, 1): {"description": "room description C"},
+                      (1, 1): {"description": "room description D"}
                       }
-        test_character = {"X-coordinate": 0, "Y-coordinate": 0, "Current HP": 5}
+        test_character = {"X-coordinate": 0, "Y-coordinate": 0}
         strands_of_time.location.board.describe_current_location(test_board, test_character)
-        expected = "You're in room description A\n\n"
+        expected = "room description A\n\n"
         self.assertEqual(expected, mock_stdout.getvalue())
 
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_describe_current_location_final_room(self, mock_stdout):
+    def test_describe_current_location_1_1(self, mock_stdout):
         test_board = {(0, 0): "room description A",
                       (1, 0): "room description B",
                       (0, 1): "room description C",
-                      (1, 1): "room description D"
+                      (1, 1): {"description": "room description D"}
                       }
-        test_character = {"X-coordinate": 1, "Y-coordinate": 1, "Current HP": 5}
+        test_character = {"X-coordinate": 1, "Y-coordinate": 1}
         strands_of_time.location.board.describe_current_location(test_board, test_character)
-        expected = "You're in room description D\n\n"
+        expected = "room description D\n\n"
         self.assertEqual(expected, mock_stdout.getvalue())
 
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_describe_current_location_third_room(self, mock_stdout):
-        test_board = {(0, 0): "room description A",
-                      (1, 0): "room description B",
-                      (0, 1): "room description C",
-                      (1, 1): "room description D"
+        test_board = {(0, 0): {"description": "room description A"},
+                      (1, 0): {"description": "room description B"},
+                      (0, 1): {"description": "room description C"},
+                      (1, 1): {"description": "room description D"}
                       }
-        test_character = {"X-coordinate": 0, "Y-coordinate": 1, "Current HP": 5}
+        test_character = {"X-coordinate": 0, "Y-coordinate": 1}
         strands_of_time.location.board.describe_current_location(test_board, test_character)
-        expected = "You're in room description C\n\n"
+        expected = "room description C\n\n"
         self.assertEqual(expected, mock_stdout.getvalue())
