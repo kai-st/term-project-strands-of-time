a440ae366f1aaf025904d435c2c2a8fbf43e0c1d
Kai Steingarten <ksteingarten@my.bcit.ca>
2024-12-06 23:07:42 -0800

    Update unit tests for move_character

--- strands_of_time/location/test_move_character.py
+++ strands_of_time/location/test_move_character.py
@@ -1,91 +1,127 @@
 from unittest import TestCase
 
-import strands_of_time.location.move
-from strands_of_time import game
+import move
 
 
 class TestMoveCharacter(TestCase):
 
-    def test_move_character_north_decrements_y(self):
-        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "Current HP": 5}
-        strands_of_time.location.move.move_character(test_character, "north")
+    def test_move_character_w_decrements_y(self):
+        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "level": 2}
+        move.move_character(test_character, "w")
         actual = test_character["Y-coordinate"]
         expected = 2
         self.assertEqual(expected, actual)
 
-    def test_move_character_north_does_not_change_x(self):
-        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "Current HP": 5}
-        strands_of_time.location.move.move_character(test_character, "north")
+    def test_move_character_w_does_not_change_x(self):
+        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "level": 2}
+        move.move_character(test_character, "w")
         actual = test_character["X-coordinate"]
         expected = 3
         self.assertEqual(expected, actual)
 
-    def test_move_character_north_does_not_change_hp(self):
-        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "Current HP": 5}
-        strands_of_time.location.move.move_character(test_character, "north")
-        actual = test_character["Current HP"]
-        expected = 5
+    def test_move_character_w_does_not_change_level(self):
+        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "level": 2}
+        move.move_character(test_character, "w")
+        actual = test_character["level"]
+        expected = 2
         self.assertEqual(expected, actual)
 
-    def test_move_character_south_increments_y(self):
-        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "Current HP": 5}
-        strands_of_time.location.move.move_character(test_character, "south")
+    def test_move_character_s_increments_y(self):
+        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "level": 2}
+        move.move_character(test_character, "s")
         actual = test_character["Y-coordinate"]
         expected = 4
         self.assertEqual(expected, actual)
 
-    def test_move_character_south_does_not_change_x(self):
-        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "Current HP": 5}
-        strands_of_time.location.move.move_character(test_character, "south")
+    def test_move_character_s_does_not_change_x(self):
+        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "level": 2}
+        move.move_character(test_character, "s")
         actual = test_character["X-coordinate"]
         expected = 3
         self.assertEqual(expected, actual)
 
-    def test_move_character_south_does_not_change_hp(self):
-        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "Current HP": 5}
-        strands_of_time.location.move.move_character(test_character, "south")
-        actual = test_character["Current HP"]
-        expected = 5
+    def test_move_character_s_does_not_change_level(self):
+        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "level": 2}
+        move.move_character(test_character, "s")
+        actual = test_character["level"]
+        expected = 2
         self.assertEqual(expected, actual)
 
-    def test_move_character_west_decrements_x(self):
-        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "Current HP": 5}
-        strands_of_time.location.move.move_character(test_character, "west")
+    def test_move_character_a_decrements_x(self):
+        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "level": 2}
+        move.move_character(test_character, "a")
         actual = test_character["X-coordinate"]
         expected = 2
         self.assertEqual(expected, actual)
 
-    def test_move_character_west_does_not_change_y(self):
-        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "Current HP": 5}
-        strands_of_time.location.move.move_character(test_character, "west")
+    def test_move_character_a_does_not_change_y(self):
+        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "level": 2}
+        move.move_character(test_character, "a")
         actual = test_character["Y-coordinate"]
         expected = 3
         self.assertEqual(expected, actual)
 
-    def test_move_character_west_does_not_change_hp(self):
-        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "Current HP": 5}
-        strands_of_time.location.move.move_character(test_character, "west")
-        actual = test_character["Current HP"]
-        expected = 5
+    def test_move_character_a_does_not_change_level(self):
+        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "level": 2}
+        move.move_character(test_character, "a")
+        actual = test_character["level"]
+        expected = 2
         self.assertEqual(expected, actual)
 
-    def test_move_character_east_increments_x(self):
-        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "Current HP": 5}
-        strands_of_time.location.move.move_character(test_character, "east")
+    def test_move_character_d_increments_x(self):
+        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "level": 2}
+        move.move_character(test_character, "d")
         actual = test_character["X-coordinate"]
         expected = 4
         self.assertEqual(expected, actual)
 
-    def test_move_character_east_does_not_change_y(self):
-        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "Current HP": 5}
-        strands_of_time.location.move.move_character(test_character, "east")
+    def test_move_character_d_does_not_change_y(self):
+        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "level": 2}
+        move.move_character(test_character, "d")
         actual = test_character["Y-coordinate"]
         expected = 3
         self.assertEqual(expected, actual)
 
-    def test_move_character_east_does_not_change_hp(self):
-        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "Current HP": 5}
-        strands_of_time.location.move.move_character(test_character, "east")
-        actual = test_character["Current HP"]
+    def test_move_character_d_does_not_change_level(self):
+        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "level": 2}
+        move.move_character(test_character, "d")
+        actual = test_character["level"]
+        expected = 2
+        self.assertEqual(expected, actual)
+
+    def test_move_character_jump_does_not_change_level(self):
+        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "level": 2}
+        move.move_character(test_character, "4d2s")
+        actual = test_character["level"]
+        expected = 2
+        self.assertEqual(expected, actual)
+
+    def test_move_character_jump_increments_x(self):
+        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "level": 2}
+        move.move_character(test_character, "4d2s")
+        actual = test_character["X-coordinate"]
+        expected = 7
+        self.assertEqual(expected, actual)
+
+
+    def test_move_character_jump_increments_y(self):
+        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "level": 2}
+        move.move_character(test_character, "4d2s")
+        actual = test_character["Y-coordinate"]
         expected = 5
+        self.assertEqual(expected, actual)
+
+    def test_move_character_jump_decrements_x(self):
+        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "level": 2}
+        move.move_character(test_character, "2a2s")
+        actual = test_character["X-coordinate"]
+        expected = 1
+        self.assertEqual(expected, actual)
+
+
+    def test_move_character_jump_decrements_y(self):
+        test_character = {"X-coordinate": 3, "Y-coordinate": 3, "level": 2}
+        move.move_character(test_character, "4d2w")
+        actual = test_character["Y-coordinate"]
+        expected = 1
         self.assertEqual(expected, actual)
